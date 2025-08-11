6ceaa5693da966c144dc722894409fca2369d6db
Srayan Jana <valorzard.kiko@outlook.com>
2025-08-11 10:15:15 -0700

    got multiple functiosn loading

--- dotnet_project/Program.cs
+++ dotnet_project/Program.cs
@@ -1,2 +1,21 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello from DotNet!");
+﻿using System;
+using System.Runtime.InteropServices;
+
+namespace dotnet_project {
+    public static class Program {
+        public delegate void HelloWorld1Delegate();
+        public static void HelloWorld1() {
+            Console.WriteLine("Hello 1 from C#!");
+        }
+
+        [UnmanagedCallersOnly]
+        public static void HelloWorld2() {
+            Console.WriteLine("Hello 2 from C#!");
+        }
+        
+        public static int HelloWorld3(IntPtr arg, int argLength) {
+            Console.WriteLine("Hello 3 from C#!");
+            return 0;
+        }
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -5,12 +5,37 @@ mod hello;
 
 fn main() {
     let hostfxr = nethost::load_hostfxr().unwrap();
-    let context = hostfxr
-        .initialize_for_dotnet_command_line(pdcstr!(
+    let context = hostfxr.initialize_for_runtime_config(pdcstr!("dotnet_project\\bin\\Debug\\net9.0\\dotnet_project.runtimeconfig.json")).unwrap();
+    let delegate_loader = context
+        .get_delegate_loader_for_assembly(pdcstr!(
             "dotnet_project\\bin\\Debug\\net9.0\\dotnet_project.dll"
         ))
         .unwrap();
-    context.run_app().as_hosting_exit_code().unwrap();
+
+    let hello_world1 = delegate_loader
+        .get_function::<fn()>(
+            pdcstr!("dotnet_project.Program, dotnet_project"),
+            pdcstr!("HelloWorld1"),
+            pdcstr!("dotnet_project.Program+HelloWorld1Delegate, dotnet_project"),
+        )
+        .unwrap();
+    hello_world1();
+
+    let hello_world2 = delegate_loader
+        .get_function_with_unmanaged_callers_only::<fn()>(
+            pdcstr!("dotnet_project.Program, dotnet_project"),
+            pdcstr!("HelloWorld2"),
+        )
+        .unwrap();
+    hello_world2();
+
+    let hello_world3 = delegate_loader
+        .get_function_with_default_signature(
+            pdcstr!("dotnet_project.Program, dotnet_project"),
+            pdcstr!("HelloWorld3"),
+        )
+        .unwrap();
+    unsafe { hello_world3(std::ptr::null(), 0) };
     
     App::new()
         .add_plugins(DefaultPlugins)
