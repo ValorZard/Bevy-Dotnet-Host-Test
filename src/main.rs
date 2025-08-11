80a619c80a24bdef6fe15b7f2b0332906d894d9d
Srayan Jana <valorzard.kiko@outlook.com>
2025-08-11 10:04:12 -0700

    get hello from dotnet

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,17 @@
 use bevy::prelude::*;
+use netcorehost::{nethost, pdcstr};
 
 mod hello;
 
 fn main() {
+    let hostfxr = nethost::load_hostfxr().unwrap();
+    let context = hostfxr
+        .initialize_for_dotnet_command_line(pdcstr!(
+            "dotnet_project\\bin\\Debug\\net9.0\\dotnet_project.dll"
+        ))
+        .unwrap();
+    context.run_app().as_hosting_exit_code().unwrap();
+    
     App::new()
         .add_plugins(DefaultPlugins)
         .add_plugins(hello::HelloPlugin)
